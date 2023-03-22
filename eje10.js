42e0df9b8a5932c9e4e03cab2e02103f1114e6ca
Armand-28 <armandopp2809@gmail.com>
2023-03-22 11:50:32 -0500

    version 22 marzo

--- eje10.js
+++ eje10.js
@@ -1,10 +1,8 @@
 let estacionamiento = 1500;
 let horas = prompt("Ingrese las horas de estacionamiento");
 let minutos = prompt("Ingrese los minutos");
-let = estacionamiento = horas * estacionamiento
-horas++;
+let = estacionamiento = horas * estacionamiento * minutos, horas++;
 
-if (estacionamiento = horas && estacionamiento = minutos);{
-    document.write("El costo de estacionamento es :",estacionamiento)
+if (estacionamiento * horas == estacionamiento && estacionamiento * minutos > 30 == horas);{
+    document.write("El costo de estacionamento es :",estacionamiento);
 }
-
