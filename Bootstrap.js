e785dc31ae49809c88114566330a1120573716ea
Armand-28 <armandopp2809@gmail.com>
2023-03-30 11:47:51 -0500

    version 30 marzo

---
+++ Bootstrap.js
@@ -0,0 +1,1 @@
+var modoscuro = document.querySelector("#modoscuro");
---
+++ practica.js
@@ -0,0 +1,41 @@
+// la tienda de lucho ofrece a los clientes que comparn harina una serie de descuento para la marca de harina  has de oroz otorga el 5 % para la marca diana otorga el 4 % de descuento, para la marca pan otorga el 2 % don lucho le solicita que se pueda identificar el nombre del cliente completo y numero de contacto y los descuentos aplicados
+
+
+let harinas = prompt("seleccione la harinas que desea comprar[haz de oros , diana , pan]")
+let hazdeoro = 2000;
+let diana = 3000;
+let pan = 2500;
+
+if (hazdeoro == 2000 / 0.05);
+
+
+
+
+
+
+
+
+
+
+// let harina = [haz_de_oro = 2000, diana = 2500, pan = 3000];
+
+// let nombre = prompt("Por favor escriba su nombre compelto: ")
+// let telefono = prompt("Por favor escriba su numero de telefono: ")
+// let costo = (" haz_de_oro")
+// let datos = (nombre + " " + telefono)
+// document.write(datos,"<br>")
+
+
+
+
+// let haz_de_oro = haz_de_oro / 0.05 * 100;
+// let diana =  diana / 0.04 * 100; 
+// let pan =  pan / 0.02 * 100;
+
+// let descuento = harina - descuento;
+
+
+
+
+
+
